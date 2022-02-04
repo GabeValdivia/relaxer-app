9a2724a515f9b57a1037dabe953dc3127cd37a16
GabeValdivia <gabe@gabeswebcoding.com>
2022-02-03 21:05:56 -0600

    Write breatheAnimation() to setTimeout for holding breathe and breathing out. Use log out to console for testing

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const text = document.getElementById('text');
 const totalTime = 7500;
 const breatheTime =  (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
+
+function breathAnimation() {
+	console.log('Breathe In');
+
+	setTimeout(() => {
+		console.log("Hold");
+
+		setTimeout(() => {
+			console.log("Breathe Out");
+		}, holdTime);
+	}, breatheTime);
+}
+
+breathAnimation();
