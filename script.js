a426055b6e03b1f333f41194b34adcc8b0d04d9a
GabeValdivia <gabe@gabeswebcoding.com>
2022-02-03 21:09:25 -0600

    Write setInterval() function to sync time with totalTime

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ const totalTime = 7500;
 const breatheTime =  (totalTime / 5) * 2;
 const holdTime = totalTime / 5;
 
+breathAnimation();
+
 function breathAnimation() {
-	console.log('Breathe In');
+	text.innerText = "Breath In";
 
 	setTimeout(() => {
-		console.log("Hold");
+		text.innerText = "Hold";
 
 		setTimeout(() => {
-			console.log("Breathe Out");
+			text.innerText = "Breathe out";
 		}, holdTime);
 	}, breatheTime);
 }
 
-breathAnimation();
+setInterval(breathAnimation, totalTime);
